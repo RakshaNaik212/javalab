ec5fe76d19aa2731d35bf92867ac2378e8d39b39
RakshaNaik212 <raksha.23cs111@sode-edu.in>
2024-11-14 10:58:16 +0530

    Add files via upload

---
+++ TestTools2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2;
+
+import pkg1.Tools;
+
+/**
+ *
+ * @author smv
+ */
+public class TestTools2 {
+    public static void main(String[] args){
+        Tools t1=new Tools();
+        t1.numbers(10);
+        t1.even(10);
+        t1.odd(10);
+        t1.mul13(10);
+        t1.mul14(10);
+        t1.mul15(10);
+       
+        
+    }
+    
+}
