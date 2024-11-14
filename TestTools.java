f3b1f296590a3a7c3c32d82becffa4a574d33d53
RakshaNaik212 <raksha.23cs111@sode-edu.in>
2024-11-14 10:19:01 +0530

    Add files via upload

---
+++ TestTools.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1;
+
+/**
+ *
+ * @author smv
+ */
+public class TestTools {
+    public static void main(String[] args){
+        Tools t1=new Tools();
+        t1.numbers(10);
+        t1.even(10);
+        t1.odd(10);
+        t1.mul13(10);
+        t1.mul14(10);
+        t1.mul15(10);
+       
+        
+    }
+}
---
+++ Tools.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1;
+
+
+public class Tools {
+    public void numbers(int n1){
+        for (int i=0;i<=n1;i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public void even(int n1){
+        for (int i=0;i<=n1;i++){
+            System.out.print(i*2+" ");
+        }
+        System.out.println();
+    }
+    public void odd(int n1){
+        for (int i=0;i<=n1;i++){
+            System.out.print(2*i+1+" ");
+        }
+        System.out.println();
+    }
+    public void mul13(int n1){
+        for (int i=0;i<=n1;i++){
+            System.out.print(i*3+" ");
+        }
+        System.out.println();
+    }
+    public void mul14(int n1){
+        for (int i=0;i<=n1;i++){
+            System.out.print(i*4+" ");
+        }
+        System.out.println();
+    }
+    public void mul15(int n1){
+        for (int i=0;i<=n1;i++){
+            System.out.print(i*5+" ");
+        }
+        System.out.println();
+    }
+    
+        
+    }
+        
